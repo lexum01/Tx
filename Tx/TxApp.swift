0d104ecdb5b13d29d0343b85cd612eddff94da02
Liam Williams <sec0gra@icloud.com>
2024-01-01 01:52:05 -0500

    Initial Commit

---
+++ Tx/TxApp.swift
@@ -0,0 +1,17 @@
+//
+//  TxApp.swift
+//  Tx
+//
+//  Created by Liam on 1/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
